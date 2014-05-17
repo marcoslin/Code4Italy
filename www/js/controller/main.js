bb4c3e59b927912494be79ef0f39184b5857a306
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-17 22:55:52 +0200

    Added additional query for deputato

--- www/js/controller/main.js
+++ www/js/controller/main.js
@@ -31,8 +31,8 @@
     app.controller("DeputatoController", ["$scope", "$routeParams", "SPARQL", "$log", function ($scope, $routeParams, SPARQL, $log) {
         //DeputatoController
         var collegio = $routeParams.collegio,
-            sql_pre = "select distinct ?deputato where { ?deputato a ocd:deputato; ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_17>; ocd:rif_mandatoCamera ?mandato . ?mandato ocd:rif_elezione ?elezione . ?elezione dc:coverage \"",
-            sql_pos = "\" . FILTER NOT EXISTS{ ?mandato ocd:endDate ?date } }",
+            sql_pre = "select distinct ?deputato ?nome ?cognome ?img ?categoryY ?categoryX where { ?deputato a ocd:deputato; foaf:firstName ?nome; foaf:surname ?cognome; foaf:gender ?categoryY ; foaf:depiction ?img; ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_17>; ocd:rif_mandatoCamera ?mandato . ?mandato ocd:rif_elezione ?elezione . ?elezione dc:coverage \"",
+            sql_pos = "\" FILTER NOT EXISTS{ ?mandato ocd:endDate ?date } ?deputato ocd:aderisce ?aderisce . ?aderisce ocd:rif_gruppoParlamentare ?gruppo . ?gruppo <http://purl.org/dc/terms/alternative> ?categoryX . MINUS{?aderisce ocd:endDate ?fineAdesione} }",
             sql_deputato = sql_pre + collegio + sql_pos
         ;
 
