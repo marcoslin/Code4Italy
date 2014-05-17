b9f44793fd32ae05b813418fda37fb53f9b441c7
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-17 18:58:05 +0200

    Changed the collegio to return an object instead of array

--- www/js/controller/main.js
+++ www/js/controller/main.js
@@ -4,12 +4,24 @@
     var app = angular.module("c4iapp");
 
     app.controller("HomeController", ["$scope", "SPARQL", "$log", function ($scope, SPARQL, $log) {
-        var sql_collegio = "select+count%28distinct+%3Fdeputato%29+%3Fcollegio+%0D%0Awhere+%7B%0D%0A++++%3Fdeputato+a+ocd%3Adeputato%3B+ocd%3Arif_leg+%3Chttp%3A%2F%2Fdati.camera.it%2Focd%2Flegislatura.rdf%2Frepubblica_17%3E%3B+ocd%3Arif_mandatoCamera+%3Fmandato+.%0D%0A++++%3Fmandato+ocd%3Arif_elezione+%3Felezione+.%0D%0A++++%3Felezione+dc%3Acoverage+%3Fcollegio+.%0D%0A++++FILTER+NOT+EXISTS%7B%0D%0A++++++++%3Fmandato+ocd%3AendDate+%3Fdate%0D%0A++++%7D%0D%0A%7D%0D%0Agroup+by+%3Fcollegio";
+        var sql_collegio = "select count(distinct ?deputato) as ?count ?collegio where { ?deputato a ocd:deputato; ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_17>; ocd:rif_mandatoCamera ?mandato . ?mandato ocd:rif_elezione ?elezione . ?elezione dc:coverage ?collegio . FILTER NOT EXISTS{ ?mandato ocd:endDate ?date } } group by ?collegio";
 
         SPARQL.getData(sql_collegio).then(function (data) {
-            $log.log("Data: ", data);
 
-            $scope.result = data.data.results.bindings;
+            var in_data = data.data.results.bindings,
+                in_data_count = in_data.length,
+                result = {}
+                ;
+
+            for (var i=0; i < in_data_count; i += 1) {
+                var key = in_data[i].collegio.value,
+                    val = in_data[i].count.value;
+                $log.log("Key: ", key, "; Value: ", val);
+                result[key] = val;
+
+            }
+
+            $scope.result = result;
 
             $log.log("Result: ", $scope.result);
 
