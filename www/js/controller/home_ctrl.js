f2a6a6086c8d6831c70daafecc9361ca6771d27d
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-18 13:43:45 +0200

    Added navigation function

--- www/js/controller/home_ctrl.js
+++ www/js/controller/home_ctrl.js
@@ -2,7 +2,7 @@
 
     var app = angular.module("c4iapp");
 
-    app.controller("HomeController", ["$scope", "SPARQL", "$log", function ($scope, SPARQL, $log) {
+    app.controller("HomeController", ["$scope", "SPARQL", "$location", "$log", function ($scope, SPARQL, $location, $log) {
         var sql_collegio = "select count(distinct ?deputato) as ?count ?collegio where { ?deputato a ocd:deputato; ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_17>; ocd:rif_mandatoCamera ?mandato . ?mandato ocd:rif_elezione ?elezione . ?elezione dc:coverage ?collegio . FILTER NOT EXISTS{ ?mandato ocd:endDate ?date } } group by ?collegio";
 
         SPARQL.getData(sql_collegio).then(function (data) {
@@ -25,6 +25,12 @@
             $log.log("Result: ", $scope.result);
 
         });
+
+        $scope.goToCollegio = function (collegio) {
+            $log.log("goToCollegio: ", collegio);
+            $location.path("deputato").search("collegio", collegio);
+        };
+
     }]);
 
 })();
