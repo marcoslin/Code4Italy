5042e110f5aca369ee4cd3492a843fd874730986
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-18 14:12:15 +0200

    latest version with map and face table

---
+++ js/controller/home_ctrl.js
@@ -0,0 +1,37 @@
+(function () {
+
+    var app = angular.module("c4iapp");
+
+    app.controller("HomeController", ["$scope", "SPARQL", "$location", "$log", function ($scope, SPARQL, $location, $log) {
+        var sql_collegio = "select count(distinct ?deputato) as ?count ?collegio where { ?deputato a ocd:deputato; ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_17>; ocd:rif_mandatoCamera ?mandato . ?mandato ocd:rif_elezione ?elezione . ?elezione dc:coverage ?collegio . FILTER NOT EXISTS{ ?mandato ocd:endDate ?date } } group by ?collegio";
+
+        SPARQL.getData(sql_collegio).then(function (data) {
+
+            var in_data = data.data.results.bindings,
+                in_data_count = in_data.length,
+                result = {}
+                ;
+
+            for (var i=0; i < in_data_count; i += 1) {
+                var key = in_data[i].collegio.value,
+                    val = in_data[i].count.value;
+                $log.log("Key: ", key, "; Value: ", val);
+                result[key] = val;
+
+            }
+
+            $scope.result = result;
+
+            $log.log("Result: ", $scope.result);
+
+        });
+
+        $scope.goToCollegio = function (collegio) {
+            $log.log("goToCollegio: ", collegio);
+            $location.path("deputato").search("collegio", collegio);
+        };
+
+    }]);
+
+})();
+
