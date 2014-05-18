923834a2f8a30fd7818b352cca65664bece50eb5
joaobiriba <glaquidara@gmail.com>
2014-05-18 16:00:31 +0200

    Added Android App

---
+++ Camera4DummiesAndroidApp/camera4dummiesandroid/src/main/java/ora/camera4dummies/camera4dummiesandroid/LeftAndRightActivity.java
@@ -0,0 +1,60 @@
+package ora.camera4dummies.camera4dummiesandroid;
+
+/**
+ * Created by joaobiriba on 17/05/14.
+ */
+import android.os.Bundle;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+
+public class LeftAndRightActivity extends BaseActivity {
+
+    public LeftAndRightActivity() {
+        super(R.string.left_and_right);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSlidingMenu().setMode(SlidingMenu.LEFT_RIGHT);
+        getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        setContentView(R.layout.content_frame);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, new MainSparqlQuery())
+                .commit();
+
+
+      getSlidingMenu().setMenu(R.layout.menu_frame_three);
+        getSlidingMenu().setShadowDrawable(R.drawable.shadowleft);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.menu_frame_three, new MapYellowFragment())
+                .commit();
+
+
+
+
+      /*  getSlidingMenu().setMenu(R.layout.interactive_map_fragment);
+        getSlidingMenu().setShadowDrawable(R.drawable.shadowleft);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.interactiveMapFragment, new ItalianInteractiveMap())
+                .commit();*/
+
+        getSlidingMenu().setSecondaryMenu(R.layout.menu_frame);
+        getSlidingMenu().setSecondaryShadowDrawable(R.drawable.shadowright);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.menu_frame, new MapFragment())
+                .commit();
+
+
+
+
+
+    }
+
+}
---
+++ Camera4DummiesAndroidApp/camera4dummiesandroid/src/main/java/ora/camera4dummies/camera4dummiesandroid/MapYellowFragment.java
@@ -0,0 +1,17 @@
+package ora.camera4dummies.camera4dummiesandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by joaobiriba on 17/05/14.
+ */
+public class MapYellowFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mapyellowfragment, null);
+    }
+
+}
