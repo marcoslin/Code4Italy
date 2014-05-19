e6fe3386eca968ee04ba1b525f5210a0f258c9e4
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-19 17:05:12 +0200

    New Sparql infra with sql in ng-template

--- js/controller/deputato_ctrl.js
+++ js/controller/deputato_ctrl.js
@@ -1,19 +1,18 @@
 (function () {
     var app = angular.module("c4iapp");
 
-    app.controller("DeputatoController", ["$scope", "$routeParams", "SPARQL", "$log", function ($scope, $routeParams, SPARQL, $log) {
+    app.controller("DeputatoController", ["$scope", "$routeParams", "SPARQL", "CameraData", "$log", function ($scope, $routeParams, SPARQL, CameraData, $log) {
         //DeputatoController
         var collegio = $routeParams.collegio,
-            sql_pre = "select distinct ?deputato ?nome ?cognome ?img ?categoryY ?categoryX where { ?deputato a ocd:deputato; foaf:firstName ?nome; foaf:surname ?cognome; foaf:gender ?categoryY ; foaf:depiction ?img; ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_17>; ocd:rif_mandatoCamera ?mandato . ?mandato ocd:rif_elezione ?elezione . ?elezione dc:coverage \"",
-            sql_pos = "\" FILTER NOT EXISTS{ ?mandato ocd:endDate ?date } ?deputato ocd:aderisce ?aderisce . ?aderisce ocd:rif_gruppoParlamentare ?gruppo . ?gruppo <http://purl.org/dc/terms/alternative> ?categoryX . MINUS{?aderisce ocd:endDate ?fineAdesione} }",
-            sql_deputato = sql_pre + collegio + sql_pos
-            ;
+            params = {
+                legislatura_uri: CameraData.legislatura_uri,
+                collegio: collegio
+            };
 
         $scope.collegio = collegio;
-
         $log.log("collegio: ", collegio);
 
-        SPARQL.getData(sql_deputato).then(function (data) {
+        SPARQL.getData("select_deputato", params).then(function (data) {
             var in_data = data.data.results.bindings,
                 res_deputato = [],
                 result = {},
