dd9ab9181792600befad0bc2ae5d44b431ccd21b
Camelia Boban <info@aissatechnologies.com>
2014-05-22 11:46:24 +0200

    Parsing birthDate and gender
    Parsing birthDate and gender - unique file js

---
+++ www/js/filter/filter.js
@@ -0,0 +1,38 @@
+
+//parsing data nascita, pattern="dd/MM/yyyy" 
+angular.module('c4iapp').filter('birthDate', function($filter)
+{
+	 return function(input)
+	 {
+		if(input == null){ return ""; } 
+	 
+		//var dataNascita = $filter('birthDate')(new Date(input), 'dd/MM/yyyy');
+		var anno, mese, giorno;
+
+  		anno = input.substr(0,4);
+  		mese = input.substr(4,2);
+  		giorno = input.substr(6,2);
+		var dataNascita = giorno + "/" + mese + "/" + giorno;
+	 
+		return dataNascita.toUpperCase();
+
+	 };
+});
+
+//parsing sesso(ita)
+angular.module('c4iapp').filter('gender', function($filter)
+{
+	 return function(input)
+	 {
+		if(input == null){ return ""; } 
+	 
+		var sesso;
+        if(input == 'male'){
+        	sesso = 'maschile';
+        }
+        else if(input == 'female'){
+        	sesso = 'femminile';
+        }	 
+		return sesso.toUpperCase();
+	 };
+});
