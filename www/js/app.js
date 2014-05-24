30d50ea09786e1634d114ebd7e011c25e799131a
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-24 21:09:33 +0200

    Implemented directive to crop the image to circle.  Added rounded corner to Atto's bio picture

--- www/js/app.js
+++ www/js/app.js
@@ -8,14 +8,15 @@
     });
 
     // Define application route
-    app.config(['$routeProvider', function ($routeProvider) {
+    app.config(['$routeProvider', '$sceProvider', function ($routeProvider, $sceProvider) {
+        $sceProvider.enabled(false)
+
         $routeProvider
             .when('/home', { templateUrl: "views/home.html", controller: "HomeController"} )
             .when('/deputato', { templateUrl: "views/deputato.html", controller: "DeputatoController"} )
             .when('/atto', { templateUrl: "views/atto.html", controller: "AttoController"} )
             .when('/team', { templateUrl: "views/team.html", navTab: "team"} )
             .when('/contact', { templateUrl: "views/contact.html", navTab: "contact"} )
-
 
             .when('/sparql', { templateUrl: "views/sparql.html", controller: "HomeController"} )
             .otherwise({redirectTo: '/home'})
