fc6d25f0c6f85f325482e568805af37d1a62848f
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-24 11:43:00 +0200

    Updated site with minor color update and reorganized the image directory.  Fixed the birthday filter

--- js/filter/filter.js
+++ js/filter/filter.js
@@ -1,38 +1,35 @@
 
 //parsing data nascita, pattern="dd/MM/yyyy" 
-angular.module('c4iapp').filter('birthDate', function($filter)
-{
-	 return function(input)
-	 {
-		if(input == null){ return ""; } 
-	 
-		//var dataNascita = $filter('birthDate')(new Date(input), 'dd/MM/yyyy');
-		var anno, mese, giorno;
+angular.module('c4iapp').filter('birthDate', ["$filter", "$log", function($filter, $log) {
+    return function (input) {
+        // Make sure a YYYYMMDD is passed
+        if(!/^(\d){8}$/.test(input)){
+            return "";
+        }
 
-  		anno = input.substr(0,4);
-  		mese = input.substr(4,2);
-  		giorno = input.substr(6,2);
-		var dataNascita = giorno + "/" + mese + "/" + giorno;
-	 
-		return dataNascita.toUpperCase();
+        //var dataNascita = $filter('birthDate')(new Date(input), 'dd/MM/yyyy');
+        var year = input.substr(0,4),
+            month = input.substr(4,2),
+            day = input.substr(6,2),
+            bdate = new Date(year,month,day);
 
-	 };
-});
+        return bdate.getDate() + "/" + bdate.getMonth()  + "/" + bdate.getYear();
+
+    };
+}]);
 
 //parsing sesso(ita)
-angular.module('c4iapp').filter('gender', function($filter)
-{
-	 return function(input)
-	 {
-		if(input == null){ return ""; } 
-	 
-		var sesso;
-        if(input == 'male'){
-        	sesso = 'maschile';
+angular.module('c4iapp').filter('gender', ["$filter", "$log", function($filter, $log) {
+    return function(input) {
+        if (typeof input === "undefined") {
+            return input;
+        } else if (/^male/i.test(input)) {
+            return "maschile";
+        } else if (/^female/i.test(input)) {
+            return "femminile";
+        } else {
+            $log.error("Unrecognized gender string: ", input);
+            return input;
         }
-        else if(input == 'female'){
-        	sesso = 'femminile';
-        }	 
-		return sesso.toUpperCase();
-	 };
-});
+    };
+}]);
