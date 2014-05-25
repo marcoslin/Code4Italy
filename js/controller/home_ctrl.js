95f1cea7d29445c693cc9bf006af4ba18605cf02
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-25 18:41:09 +0200

    Added welcome text on the first page and beta label

--- js/controller/home_ctrl.js
+++ js/controller/home_ctrl.js
@@ -3,7 +3,9 @@
     var app = angular.module("c4iapp");
 
     app.controller("HomeController", ["$scope", "SPARQL", "CameraData", "$location", "$log", function ($scope, SPARQL, CameraData, $location, $log) {
-
+        /*
+         * Skipping the select_collegio call for now as we are not displaying the count
+         *
         SPARQL.getData("select_collegio", CameraData).then(function (data) {
 
             var in_data = data.data.results.bindings,
@@ -24,10 +26,18 @@
             $log.log("Result: ", $scope.result);
 
         });
+        */
 
         $scope.goToCollegio = function (collegio) {
             $log.log("goToCollegio: ", collegio);
             $location.path("deputato").search("collegio", collegio);
+        };
+
+        $scope.otherRegions = {
+            "EUROPA": "Europa",
+            "AMERICA MERIDIONALE": "America Meridionale",
+            "AFRICA, ASIA, OCEANIA E ANTARTIDE": "Africa, Asia, Oceania e Antartide",
+            "AMERICA SETTENTRIONALE E CENTRALE": "America Settentrionale e Centrale"
         };
 
         // Initialize mapper.js
