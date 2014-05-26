779648ef5a5674455f216591d57a5f1f65962245
Marcos Lin <linm.linmair@ebacimail.com>
2014-05-26 09:43:54 +0200

    Added Google Analytics

--- js/directive/navigation.js
+++ js/directive/navigation.js
@@ -1,13 +1,14 @@
 (function () {
     var app = angular.module("c4iapp");
 
-    app.directive('navMenu', ["navTextUpdater", "$route", "$log", function (navTextUpdater, $route, $log) {
+    app.directive('navMenu', ["navTextUpdater", "$route", "$location", "$window", "$log", function (navTextUpdater, $route, $location, $window, $log) {
         return {
             restrict: 'EA',
             templateUrl: 'js/directive/template/navMenu.html',
             controller: function ($scope) {
-                var curTab = $route.current.navTab;
-                $log.log("curTab: ", curTab);
+                var curTab = $route.current.navTab,
+                    siteName = "/Code4Italy",
+                    ga_path = siteName + $location.path();
 
                 // Set tab
                 $scope.fs = {
@@ -36,11 +37,13 @@
                     };
                 }
 
+                // $log.log("Sending to GA: " + ga_path);
+                $window._gaq.push(['_trackPageview', ga_path]);
 
                 // Update Navigation Text
                 navTextUpdater.update(function (message) {
                     // This is called is involked from jquery.maphilight so apply is needed
-                    $log.log("Update:", message);
+                    //$log.log("Update:", message);
                     $scope.navigationText = message;
                     if (!$scope.$$phase) {
                         $scope.$apply();
